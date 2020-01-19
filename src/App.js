672a3f064da9f5c51d0826ac0757910101d44bc6
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-19 13:56:46 -0500

    created search component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt me !"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havenese"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cocktatiel"
-  //   }),
-  //
-  //   React.createElement(Pet, { name: "Doink ", animal: "Cat", breed: "Mixed" })
-  // ]);
-
   return (
     <div>
       <h1>Adopt me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havenese" />
-      <Pet name="Pepper" animal="Bird" breed="Cocktatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
