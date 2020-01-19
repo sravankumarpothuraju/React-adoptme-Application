aef994bac8d27a0fec33e4a1751fbd708f8a73fb
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-19 15:01:17 -0500

    calling the pet api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle,WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -14,6 +16,19 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={event => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
