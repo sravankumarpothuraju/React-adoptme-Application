29322d1dc40ad795aaf157ac0f0258be26f8131c
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-19 18:44:15 -0500

    using one way data binding that is props and displaying data on the page

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle,WA");
@@ -21,18 +22,20 @@ const SearchParams = () => {
   useEffect(() => {
     setBreeds([]);
     setBreed("");
-    pet.breeds(animal).then(({ apibreeds }) => {
-      const breedStrings = apibreeds.map(({ name }) => name);
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
   }, [animal, setBreeds, setBreed]);
 
   return (
     <div className="search-params">
-      <form onSubmit={(event)=>{
-        event.preventDefault();
-        requestPets();
-      }}>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -46,6 +49,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
