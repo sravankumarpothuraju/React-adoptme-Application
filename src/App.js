59f85468a3160a7aaf8ccd46f1b606f0f3d31eae
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-19 12:30:55 -0500

    installed react and react dom using npm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-//your code goes here for now
+import React from "react";
+import { render } from "react-dom";
+
 const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
@@ -25,4 +27,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
