78003523cfa57c9c03f92330d042d62cd70f98d0
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-19 23:06:28 -0500

    created image carousel component

--- src/Details.js
+++ src/Details.js
@@ -19,7 +19,21 @@ class Details extends React.Component {
   }
 
   render() {
-    return;
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { name, animal, location, description, breed } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal}-${breed}-${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
