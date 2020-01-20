37e519dd8afc86feb8991336351990bc610c844e
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-19 20:03:17 -0500

    added reach router for routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1>Adopt me</h1>
-        <SearchParams />
+        <h1 id="something-important">Adopt me</h1>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = props => {
+  return <div>hi</div>;
+};
+
+export default Details;
