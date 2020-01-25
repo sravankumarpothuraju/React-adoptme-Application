ffa4dfde5cdc220a6844b035c86da8e045376af5
SRAVAN KUMAR POTHURAJU <sravan.pothuraju@gmail.com>
2020-01-25 12:24:45 -0500

    Revert "Typescript"

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, lazy, Suspense } from "react";
+import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
+import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(()=>import("./SearchParams"))
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <div>
+
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Suspense fallback={<h1>loading route ...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
+
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
