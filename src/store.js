5196101e9e613915ebc9f017e28e36aee9b09081
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-26 23:18:33 -0500

    created reducers for location

--- src/store.js
+++ src/store.js
@@ -2,6 +2,12 @@ import { createStore } from "redux";
 
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  typeof window === "object" &&
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+);
 
 export default store;
